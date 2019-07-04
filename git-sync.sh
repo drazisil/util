a90ed6bf4a99307a3f15e745ca415faa163b67f7
Joe Becher <jwbecher@drazisil.com>
2019-07-04 07:30:33 -0400

    add git-sync

--- git-sync.sh
+++ git-sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-find /home/drazisil/github/ -type d -name .git | xargs dirname | xargs -I % sh -c 'echo "\n%";git -C % status;git -C % remote -v'
+find $HOME/github/ -type d -name .git | xargs dirname | xargs -I % sh -c 'echo "\n%";git -C % status;git -C % remote -v'
